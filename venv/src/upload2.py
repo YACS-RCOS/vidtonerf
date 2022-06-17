55ad679d68c9c0b100b263b14c2b5c7de936a868
KateW0912 <72034372+KateW0912@users.noreply.github.com>
2022-06-17 17:39:50 -0400

    Add files via upload

---
+++ venv/src/upload2.py
@@ -0,0 +1,35 @@
+import os
+#from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+	
+app = Flask(__name__)
+
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_video():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	else:
+		filename = secure_filename(file.filename)
+		file.save(filename)
+		#print('upload_video filename: ' + filename)
+		print('Video successfully uploaded and displayed below')
+		return render_template('upload.html', filename=filename)
+
+@app.route('/display/<filename>')
+def display_video(filename):
+	#print('display_video filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == "__main__":
+    app.run()
